314cca5b51e7948e4198947ec457c89ce408948f
Sean Finegan <sean.j.finegan@gmail.com>
2021-09-20 21:36:06 -0700

    testing fetch

--- js/script.js
+++ js/script.js
@@ -1,25 +1,108 @@
-$(function (){
-    $('#calculate').on('click',function() {
-   
-      let cost = $('#area').val() * $('#epoxy-type').val();
-      let dollarCost;
-      if(isNaN(cost)) {
-        console.log("test");
-        dollarCost = "Choose Epoxy Type";
-      } else {
-      // Create our number formatter.
-      var formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
+$(function () {
+    $("#calculate").on("click", function () {
+        let dist = getDistance($("#area-code").val());
 
-          // These options are needed to round to whole numbers.
-          //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-          //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-      });
-      dollarCost = formatter.format(cost);
+        // if(dist < 15){
+        //   dist = 0;
+        // } else {
+        //   dist *= 5;
+        // }
+        console.log("dist: " + dist);
+        let cost = $("#area").val() * $("#epoxy-type").val();
+        let dollarCost;
+        if (isNaN(cost)) {
+            console.log("test");
+            dollarCost = "Choose Epoxy Type";
+        } else {
+            // Create our number formatter.
+            var formatter = new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+
+                // These options are needed to round to whole numbers.
+                //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+                //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+            });
+            dollarCost = formatter.format(cost);
+        }
+        $("#estimate").html(`${dollarCost}`);
+    });
+});
+
+function getDistance(clientAreaCode) {
+    // IMPORTANT: Fill in your client key
+    var clientKey =
+        "6SXj9OSpKkBtciDlD0YJIsvIh7ah0Ca8JJptrdePNyA4anUm1UBpyOYA9fMntLuL";
+    console.log("in getDistance. Cliant area code: " + clientAreaCode);
+    var cache = {};
+    console.log(clientAreaCode);
+    /** Handle successful response */
+    function handleResp(data) {
+        // Check for error
+        if (data.error_msg) console.log(data.error_msg);
+        else if ("distance" in data) {
+            console.log("data.distance: " + data.distance);
+            return data.distance;
+        } else {
+            console.log("data: " + data);
+        }
     }
-      $('#estimate').html(`${dollarCost}`);
-    })
-  })
-  
+    // Set up event handlers
+    // Get zip code
+    var zipcode1 = "83641";
+    var zipcode2 = clientAreaCode.toString();
+    if (zipcode1.length == 5 && zipcode2.length == 5) {
+        // Check cache
+        console.log("ZIP CODES ARE CORRECT");
+        var cacheKey = zipcode1 + "-" + zipcode2;
+        if (cacheKey in cache) {
+            handleResp(cache[cacheKey]);
+            console.log("data in cache");
+        } else {
+            // Build url
+            let url =
+                "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/" +
+                clientKey +
+                "/distance.json/" +
+                zipcode1 +
+                "/" +
+                zipcode2 +
+                "/mile";
 
+				fetch(url)
+				.then((response) => {
+				  return response.json();
+				})
+				.then((data) => {
+				  console.log(data);
+				});
+
+            // Make AJAX request
+            // 			$.ajax({
+            // 				"url": url,
+            // 				"dataType": "json"
+            // 			}).done(function(data) {
+            // 				console.log("data1: " + data);
+            //     return handleResp(data);
+
+            // 				// Store in cache
+            // 				cache[cacheKey] = data;
+            // 			}).fail(function(data) {
+            //     console.log('data in cache');
+            //     console.log(data)
+            // 				if (data.responseText && (json = JSON.parse(data.responseText)))
+            // 				{
+            // 					// Store in cache
+            // 					cache[cacheKey] = json;
+            // 					console.log("data2: " + data)
+            // 					// Check for error
+            // 					if (json.error_msg)
+            // 						console.log(json.error_msg);
+            // 				}
+            // 				else
+            // 					console.log('Request failed.');
+            // 			});
+            //   console.log('end of AJAX')
+        }
+    }
+}
